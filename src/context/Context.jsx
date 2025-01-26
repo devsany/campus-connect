a65a26c9f7d21941e2209183231c263cfc52eb5e
devsany <sunnyrajbodhgaya19@gmail.com>
2025-01-26 10:50:41 +0530

    update toggle functionality tonavbar

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -6,10 +6,14 @@ export const AppContext = createContext();
 // Create a Provider component
 export const AppProvider = ({ children }) => {
   const [userToken, setUserToken] = useState("");
+  const [toggleMainNavigationLink, setToggleMainNavigationLink] =
+    useState(false);
 
   const value = {
     userToken,
     setUserToken,
+    setToggleMainNavigationLink,
+    toggleMainNavigationLink,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
--- src/navbar/MainNavbarSignIn.jsx
+++ src/navbar/MainNavbarSignIn.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { AppContext } from "../context/Context";
 
 const MainNavbarSignIn = () => {
   const [toggle, setToggle] = useState(false);
+  const { toggleMainNavigationLink, setToggleMainNavigationLink } =
+    useContext(AppContext);
 
   return (
     <div>
@@ -22,7 +25,7 @@ const MainNavbarSignIn = () => {
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               onClick={() => {
-                setToggle(!toggle);
+                setToggleMainNavigationLink(!toggleMainNavigationLink);
               }}
               id="mega-menu-full-dropdown-button"
               data-collapse-toggle="mega-menu-full-dropdown"
@@ -38,9 +41,9 @@ const MainNavbarSignIn = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 1 4 4 4-4"
                 />
               </svg>
@@ -53,34 +56,46 @@ const MainNavbarSignIn = () => {
           >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
-                  href="#"
+                <NavLink
+                  to="/"
                   className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                   aria-current="page"
                 >
-                  Course
-                </a>
+                  <div
+                    onClick={() => {
+                      setToggleMainNavigationLink(false);
+                    }}
+                  >
+                    Course
+                  </div>
+                </NavLink>
               </li>
               <li>
                 <a
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  College
+                  <div
+                    onClick={() => {
+                      setToggleMainNavigationLink(false);
+                    }}
+                  >
+                    College
+                  </div>
                 </a>
               </li>
               <li>
-                <input type="text" placeholder="Search College" />
+                {/* <input type="text" placeholder="Search College" /> */}
               </li>
               <li></li>
             </ul>
           </div>
         </div>
       </nav>
-      {toggle && (
+      {toggleMainNavigationLink && (
         <div
           id="mega-menu-full-dropdown"
-          className="mt-[70px] absolute border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600"
+          className="mt-[50px] absolute border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600"
         >
           <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
             <ul>
@@ -89,13 +104,19 @@ const MainNavbarSignIn = () => {
                   to="/sign_up_for_student"
                   className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <div className="font-semibold">
-                    Registration for New Student
+                  <div
+                    onClick={() => {
+                      setToggleMainNavigationLink(false);
+                    }}
+                  >
+                    <div className="font-semibold">
+                      Registration for New Student
+                    </div>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Unlock the door to endless opportunities—sign up and let
+                      your journey to knowledge and success begin!
+                    </span>
                   </div>
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    Unlock the door to endless opportunities—sign up and let
-                    your journey to knowledge and success begin!
-                  </span>
                 </NavLink>
               </li>
               <li>
@@ -103,11 +124,17 @@ const MainNavbarSignIn = () => {
                   to="/sign_in_for_student"
                   className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <div className="font-semibold">Sign In for Student</div>
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    Welcome back! Your path to growth and learning continues
-                    here.
-                  </span>
+                  <div
+                    onClick={() => {
+                      setToggleMainNavigationLink(false);
+                    }}
+                  >
+                    <div className="font-semibold">Sign In for Student</div>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Welcome back! Your path to growth and learning continues
+                      here.
+                    </span>
+                  </div>
                 </NavLink>
               </li>
               <li>
@@ -115,11 +142,17 @@ const MainNavbarSignIn = () => {
                   to="/contact"
                   className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <div className="font-semibold">Contact Us</div>
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    Reach out to us – we’re here to listen, support, and guide
-                    you every step of the way.
-                  </span>
+                  <div
+                    onClick={() => {
+                      setToggleMainNavigationLink(false);
+                    }}
+                  >
+                    <div className="font-semibold">Contact Us</div>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Reach out to us – we’re here to listen, support, and guide
+                      you every step of the way.
+                    </span>
+                  </div>
                 </NavLink>
               </li>
             </ul>
@@ -129,13 +162,19 @@ const MainNavbarSignIn = () => {
                   to="/sign_up_for_college"
                   className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <div className="font-semibold">
-                    Registration for New College
+                  <div
+                    onClick={() => {
+                      setToggleMainNavigationLink(false);
+                    }}
+                  >
+                    <div className="font-semibold">
+                      Registration for New College
+                    </div>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Join us in shaping the future – register your college
+                      today and be part of the educational revolution!
+                    </span>
                   </div>
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    Join us in shaping the future – register your college today
-                    and be part of the educational revolution!
-                  </span>
                 </NavLink>
               </li>
               <li>
@@ -143,11 +182,17 @@ const MainNavbarSignIn = () => {
                   to="/sign_in_for_college"
                   className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <div className="font-semibold">Sign In for College</div>
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    Welcome back! Access your college dashboard and continue
-                    empowering education.
-                  </span>
+                  <div
+                    onClick={() => {
+                      setToggleMainNavigationLink(false);
+                    }}
+                  >
+                    <div className="font-semibold">Sign In for College</div>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Welcome back! Access your college dashboard and continue
+                      empowering education.
+                    </span>
+                  </div>
                 </NavLink>
               </li>
               <li>
@@ -155,13 +200,19 @@ const MainNavbarSignIn = () => {
                   href="#"
                   className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <div className="font-semibold">
-                    Frequently Asked Questions (FAQ)
+                  <div
+                    onClick={() => {
+                      setToggleMainNavigationLink(false);
+                    }}
+                  >
+                    <div className="font-semibold">
+                      Frequently Asked Questions (FAQ)
+                    </div>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      provide quick and straightforward answers to the most
+                      common queries you might have.
+                    </span>
                   </div>
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    provide quick and straightforward answers to the most common
-                    queries you might have.
-                  </span>
                 </a>
               </li>
             </ul>
